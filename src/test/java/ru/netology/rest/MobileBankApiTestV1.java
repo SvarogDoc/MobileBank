9ac815fccb5aec8819064fc9330ce1859d8970ad
SvarogDoc <learnqai33@gmail.com>
2022-06-10 13:47:25 +0300

    modified the test V1 and schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,8 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +18,8 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+              .body("every {it.currency == 'RUB' || it.currency == 'USD'}", is (true));
     }
 }
